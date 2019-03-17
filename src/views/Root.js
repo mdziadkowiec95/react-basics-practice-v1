c6e9ba072319fdf8e34cf0bbae34d89bd7a96e33
michal.dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-03-17 13:53:21 +0100

    I have added axios and tested first request from the API

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Matches from '../components/Matches';
 
 class Root extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="Root">
+        <Matches />
       </div>
     );
   }
